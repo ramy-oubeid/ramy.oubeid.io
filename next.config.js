958bb9966fac7b9476733ab60a46220a4a166730
RAMY OUBEID <OUBEID.ELHADRAMI@GMAIL.COM>
2024-08-03 21:58:03 -0400

    Added a script for deployment

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    output: 'export',
+    // Add any other configurations you might need. For example, if deploying to GitHub Pages:
+    basePath: '/[gh-pages]',
+    assetPrefix: '/[gh-pages]/',
+    images : {
+        unoptimized: true
+    }
+};
