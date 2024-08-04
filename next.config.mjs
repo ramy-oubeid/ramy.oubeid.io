fa3cfa1a901a1b646ba4f3ecde9b9d2ce8a91f7d
RAMY OUBEID <OUBEID.ELHADRAMI@GMAIL.COM>
2024-08-04 12:06:49 -0400

    Update package.json for deployment automation

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-
 const nextConfig = {
-
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    // Add any other necessary configurations here
 };
 
 export default nextConfig;
