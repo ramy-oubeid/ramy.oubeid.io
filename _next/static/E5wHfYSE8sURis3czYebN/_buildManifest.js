3b43650ef74abe37f1c381e76f7f104856bdc8b7
RAMY OUBEID <OUBEID.ELHADRAMI@GMAIL.COM>
2024-08-03 21:52:04 -0400

    Updates

---
+++ _next/static/E5wHfYSE8sURis3czYebN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2196a3a02ce2a7de.js"],"/404":["static/chunks/pages/404-a38e3ab6bb17db31.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
